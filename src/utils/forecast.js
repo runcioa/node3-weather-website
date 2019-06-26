d841cc429ff305b9b3e421d3ff0b6539f428a1b3
Antonio Runcio <antonio.runcio@ecoforservice.it>
2019-06-26 12:06:30 +0200

    Adding forecast daily max e min Temp

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -20,7 +20,8 @@ const forecast = (latitude, longitude, callback)=>{
             
         } else {
             
-            callback(undefined, body.daily.data[0].summary + 'It is currently ' + body.currently.temperature + ' degree out. There is a ' + body.currently.precipProbability + ' % chance of rain')
+            callback(undefined, body.daily.data[0].summary + 'It is currently ' + body.currently.temperature + ' degree out. There is a ' + body.currently.precipProbability + ' % chance of rain. ' + 
+            'Temperatura massima giornaliera ' + body.daily.data[0].temperatureHigh + 'Temperatura minima giornaliera ' + body.daily.data[0].temperatureLow)
         }
         
     })
